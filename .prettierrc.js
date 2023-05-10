5edf19bf8ce9a6d4490f93daaa0175460a58b403
Krindendo work <lollipopfunkyx9@gmail.com>
2023-05-11 00:49:21 +0200

    init

---
+++ .prettierrc.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  bracketSpacing: false,
+  singleQuote: true,
+  jsxBracketSameLine: true,
+  trailingComma: 'es5',
+  printWidth: 80,
+  parser: 'flow',
+  arrowParens: 'avoid',
+  overrides: [
+    {
+      files: [
+        'packages/*/*.js',
+        // Source files
+        'packages/*/src/**/*.js',
+        'packages/shared/**/*.js',
+        'scripts/rollup/shims/**/*.js',
+      ],
+      options: {
+        trailingComma: 'all',
+      },
+    },
+  ],
+};
